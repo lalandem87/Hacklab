f5d5f9f7534b4712fb3a34bfab3e9a8b08bed2c5
Dixxh <lalandemaxime37@gmail.com>
2026-06-18 17:27:32 +0200

    Add Footer to all pages

--- hacklab/src/App.tsx
+++ hacklab/src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header/Header";
 import { Router } from "./router";
+import { Footer } from "./components/Footer/Footer";
 
 export function App() {
   return (
     <>
       <Header />
       <Router />
+      <Footer />
     </>
   );
 }
---
+++ hacklab/src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router";
+import type { JSX } from "react/jsx-runtime";
+import "./Footer.scss";
+
+export function Footer(): JSX.Element {
+  return (
+    <footer>
+      <div className="main-logo">
+        <span className="logo-icon">&gt;_</span>
+        <span className="logo-text">Hacklab</span>
+      </div>
+      <div className="links-wrapper">
+        <NavLink to="/">Confidentialité</NavLink>
+        <NavLink to="/">Mention Légales</NavLink>
+        <NavLink to="/">Contact</NavLink>
+        <NavLink to="/">Discord</NavLink>
+      </div>
+      <p>@ {new Date().getFullYear()} CyberLearn. Tous droits réservés.</p>
+    </footer>
+  );
+}
